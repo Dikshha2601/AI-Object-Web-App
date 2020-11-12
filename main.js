af914cbb8a0b33cddf827d5c0eec90595efaee70
Dikshha2601 <66013703+Dikshha2601@users.noreply.github.com>
2020-11-12 20:34:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function one() {
+  window.location = "hall.html";
+}
+
+function two() {
+  window.location = "hall copy.html";
+}
+
+function three() {
+  window.location = "hall copy 2.html";
+}
+
+function four() {
+  window.location = "hall copy 3.html";
+}
+
+function five() {
+  window.location = "hall copy 4.html";
+}
