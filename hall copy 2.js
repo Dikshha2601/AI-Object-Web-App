eb7e010d6bd44f81d6cf1ed7f960204af2ab70c5
Dikshha2601 <66013703+Dikshha2601@users.noreply.github.com>
2020-11-19 15:26:15 +0530

    Add files via upload

--- hall copy 2.js
+++ hall copy 2.js
@@ -32,19 +32,19 @@ function setup() {
 
 function draw() {
   image(img, 0, 0, 640, 480);
-  // if (status != "") {
-  //   document.getElementById("status").innerHTML = "Status: Objects Detected";
-  //   for (i = 0; i < objects.length; i++) {
-  //     fill("#000000");
-  //     percent = floor(objects[i].confidence * 100);
-  //     text(
-  //       objects[i].label + " " + percent + "%",
-  //       objects[i].x + 15,
-  //       objects[i].y + 15
-  //     );
-  //     noFill();
-  //     stroke("#000000");
-  //     rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-  //   }
-  // }
+  if (status != "") {
+    document.getElementById("status").innerHTML = "Status: Objects Detected";
+    for (i = 0; i < objects.length; i++) {
+      fill("#000000");
+      percent = floor(objects[i].confidence * 100);
+      text(
+        objects[i].label + " " + percent + "%",
+        objects[i].x + 15,
+        objects[i].y + 15
+      );
+      noFill();
+      stroke("#000000");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
 }
